82bda4a922f2a0dac13460e9b187eec9aed96f14
khandelwalrahul123 <khandelwalrahul920@gmail.com>
2020-04-09 21:51:58 +0530

    Initial commit of project

---
+++ src/main/java/assignment/week1/maven/HelloWorld.java
@@ -0,0 +1,10 @@
+package assignment.week1.maven;
+
+
+public class HelloWorld 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/assignment/week1/maven/HelloWorldTest.java
@@ -0,0 +1,16 @@
+package assignment.week1.maven;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class HelloWorldTest extends TestCase
+{
+    public void testHelloWorld()
+    {
+        assertTrue( true );
+    }
+}
